7b73278a49ef5f249f887f8e405ca7e1ce760f55
Damilola Adekoya <dharmykoya38@gmail.com>
2021-04-26 12:50:01 +0100

    chore(sequelize):setup sequelize and user model

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,26 +1,28 @@
 require("dotenv").config();
 
-export const development = {
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  logging: false,
-  dialect: "postgres"
-};
-export const test = {
-  username: process.env.TEST_DATABASE_USERNAME,
-  password: process.env.TEST_DATABASE_PASSWORD,
-  database: process.env.TEST_DATABASE_NAME,
-  host: process.env.TEST_DATABASE_HOST,
-  logging: false,
-  dialect: "postgres"
-};
-export const production = {
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  logging: false,
-  dialect: "postgres"
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    logging: false,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.TEST_DATABASE_USERNAME,
+    password: process.env.TEST_DATABASE_PASSWORD,
+    database: process.env.TEST_DATABASE_NAME,
+    host: process.env.TEST_DATABASE_HOST,
+    logging: false,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    logging: false,
+    dialect: "postgres"
+  }
 };
